d0a2dfb3c24a98a8a9ed99271eba359f481c0064
sabbirgit <msa29.cse@gmail.com>
2024-07-30 00:53:22 +0600

    A string problem solution by me

---
+++ 71A-Way-Too-Long-Words.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+	char str[100][100];
+	int n;
+	scanf("%d",&n);
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%s",str[i]);
+	}
+	
+	int length;
+	for (int i = 0; i < n; i++)
+	{
+		length = strlen(str[i]);
+		if (length>10)
+		{
+			//printf("%s",str[0],"%d", length-2, "%s", str[length-1]);
+			printf("%c%d%c\n", str[i][0], length - 2, str[i][length - 1]);
+			
+		}
+		else {
+			printf("%s\n", str[i]);
+		}
+		
+		//printf("%d\n", length);
+	}
+	
+	return 0;
+}
