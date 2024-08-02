b734a31ae4dcc9143f0546fcfc560823450863a7
Sabbir Ahmed <69301433+sabbirahmed404@users.noreply.github.com>
2024-08-02 13:13:43 +0600

    Add files via upload
    problem 158A

---
+++ 158A-the-next-round.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int n,k,var[100],flag=0;
+    scanf("%d %d", &n, &k);
+    for (size_t i = 0; i < n; i++)
+    {
+        scanf("%d",&var[i]);
+    }
+
+    for (size_t i = 0; i < n; i++)
+    {
+        if (var[k]<=var[i])
+        {
+            flag++;
+        }
+        
+    }
+    printf("%d",flag);
+    
+
+    return 0;
+}
