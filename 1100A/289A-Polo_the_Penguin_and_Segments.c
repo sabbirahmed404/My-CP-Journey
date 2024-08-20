431bfd5a6ee3e054610fe147f0c2be5e1d0396db
Sabbir Ahmed <69301433+sabbirahmed404@users.noreply.github.com>
2024-08-20 20:51:41 +0600

    Create 289A-Polo_the_Penguin_and_Segments.c

---
+++ 1100A/289A-Polo_the_Penguin_and_Segments.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    scanf("%d %d", &n, &k);
+
+    int total_length = 0;
+    
+    for (int i = 0; i < n; i++) {
+        int l, r;
+        scanf("%d %d", &l, &r);
+        total_length += (r - l + 1);
+    }
+    
+    int remainder = total_length % k;
+    int moves = (remainder == 0) ? 0 : k - remainder;
+    
+    printf("%d\n", moves);
+    
+    return 0;
+}#include <stdio.h>
+
+int main() {
+    int n, k;
+    scanf("%d %d", &n, &k);
+
+    int total_length = 0;
+    
+    for (int i = 0; i < n; i++) {
+        int l, r;
+        scanf("%d %d", &l, &r);
+        total_length += (r - l + 1);
+    }
+    
+    int remainder = total_length % k;
+    int moves = (remainder == 0) ? 0 : k - remainder;
+    
+    printf("%d\n", moves);
+    
+    return 0;
+}
