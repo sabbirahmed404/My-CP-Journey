4d3ef1044d15c5a7973b8a0c009e27b883720e09
Sabbir Ahmed <69301433+sabbirahmed404@users.noreply.github.com>
2024-08-21 23:56:10 +0600

    Update 289A-Polo_the_Penguin_and_Segments.c

--- 1100A/289A-Polo_the_Penguin_and_Segments.c
+++ 1100A/289A-Polo_the_Penguin_and_Segments.c
@@ -23,7 +23,6 @@ int main() {
 int main() {
     int n, k;
     scanf("%d %d", &n, &k);
-
     int total_length = 0;
     
     for (int i = 0; i < n; i++) {
@@ -31,10 +30,8 @@ int main() {
         scanf("%d %d", &l, &r);
         total_length += (r - l + 1);
     }
-    
     int remainder = total_length % k;
     int moves = (remainder == 0) ? 0 : k - remainder;
-    
     printf("%d\n", moves);
     
     return 0;
