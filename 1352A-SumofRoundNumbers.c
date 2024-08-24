bb8c8a86a452b5f7258fa35b2b6b9ec8aa7b6178
Sabbir Ahmed <69301433+sabbirahmed404@users.noreply.github.com>
2024-08-24 23:43:54 +0600

    Create 1352A-SumofRoundNumbers.c

---
+++ 1352A-SumofRoundNumbers.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void find_round_numbers(int n) {
+    int position = 1;
+    int result[5]; // Maximum 5 digits in 104, hence at most 5 round numbers
+    int count = 0;
+    
+    while (n > 0) {
+        int digit = n % 10;
+        if (digit != 0) {
+            result[count++] = digit * position;
+        }
+        n /= 10;
+        position *= 10;
+    }
+    
+    printf("%d\n", count);
+    for (int i = 0; i < count; i++) {
+        printf("%d ", result[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    
+    while (t--) {
+        int n;
+        scanf("%d", &n);
+        find_round_numbers(n);
+    }
+    
+    return 0;
+}
