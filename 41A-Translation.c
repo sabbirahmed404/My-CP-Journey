3bdf478c5efb2126ca692d5b30a94d2d6d346736
Sabbir Ahmed <69301433+sabbirahmed404@users.noreply.github.com>
2024-08-26 23:13:42 +0600

    Create 41A-Translation.c

---
+++ 41A-Translation.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[101], t[101];
+    scanf("%s", s);
+    scanf("%s", t);
+
+    int len_s = strlen(s);
+    int len_t = strlen(t);
+
+    if (len_s != len_t) {
+        printf("NO\n");
+        return 0;
+    }
+
+    for (int i = 0; i < len_s; i++) {
+        if (s[i] != t[len_t - i - 1]) {
+            printf("NO\n");
+            return 0;
+        }
+    }
+
+    printf("YES\n");
+    return 0;
+}
