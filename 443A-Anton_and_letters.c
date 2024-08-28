cf8890d319ff25011dd36a279cf1f3e3a9684952
Sabbir Ahmed <69301433+sabbirahmed404@users.noreply.github.com>
2024-08-28 23:33:00 +0600

    Create 443A-Anton_and_letters.c

---
+++ 443A-Anton_and_letters.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main() {
+    char input[1001];
+    fgets(input, sizeof(input), stdin);
+
+    bool letters[26] = {false};  // to keep track of distinct letters
+    int distinct_count = 0;
+
+    for (int i = 0; i < strlen(input); i++) {
+        if (input[i] >= 'a' && input[i] <= 'z') {
+            if (!letters[input[i] - 'a']) {
+                letters[input[i] - 'a'] = true;
+                distinct_count++;
+            }
+        }
+    }
+
+    printf("%d\n", distinct_count);
+    return 0;
+}
