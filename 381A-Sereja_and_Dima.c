d0f05ee87486b7f2c7e71f534f7ba0ec9fd1da04
Sabbir Ahmed <69301433+sabbirahmed404@users.noreply.github.com>
2024-08-30 22:24:11 +0600

    Create 381A-Sereja_and_Dima.c

---
+++ 381A-Sereja_and_Dima.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    int cards[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &cards[i]);
+    }
+    
+    int sereja = 0, dima = 0;
+    int left = 0, right = n - 1;
+    
+    for (int turn = 0; left <= right; turn++) {
+        if (cards[left] > cards[right]) {
+            if (turn % 2 == 0) {
+                sereja += cards[left];
+            } else {
+                dima += cards[left];
+            }
+            left++;
+        } else {
+            if (turn % 2 == 0) {
+                sereja += cards[right];
+            } else {
+                dima += cards[right];
+            }
+            right--;
+        }
+    }
+    
+    printf("%d %d\n", sereja, dima);
+    
+    return 0;
+}
