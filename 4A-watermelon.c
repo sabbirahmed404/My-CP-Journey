b6699d4f87f3ede217740f802e3a31b092c932b6
Sabbir Ahmed <69301433+sabbirahmed404@users.noreply.github.com>
2024-09-03 22:54:54 +0600

    Update 4A-watermelon.c

--- 4A-watermelon.c
+++ 4A-watermelon.c
@@ -3,12 +3,10 @@
 int main() {
     int v;
     scanf("%d", &v);
-    
     if (v > 2 && v % 2 == 0) {
         printf("YES\n");
     } else {
         printf("NO\n");
-    }
     
     return 0;
 }
