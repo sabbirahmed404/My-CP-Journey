e7568e33ff4a392b84c7d033cb5859f06182e05b
Sabbir Ahmed <69301433+sabbirahmed404@users.noreply.github.com>
2024-09-04 23:45:07 +0600

    Create 344A-Magnets.c

---
+++ 344A-Magnets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n); // Read the number of magnets
+    
+    char current[3]; // To hold the current magnet orientation
+    char previous[3]; // To hold the previous magnet orientation
+    int groups = 0; // Count of groups
+
+    // Read the first magnet
+    scanf("%s", current);
+    groups = 1; // The first magnet always starts a new group
+
+    for (int i = 1; i < n; i++) {
+        scanf("%s", previous); // Read the next magnet
+
+        // If the current magnet is different from the previous one
+        if (previous[0] != current[0]) {
+            groups++; // A new group is formed
+        }
+        
+        // Update current to previous for the next iteration
+        current[0] = previous[0];
+        current[1] = previous[1];
+        current[2] = '\0'; // Null terminate the string
+    }
+
+    printf("%d\n", groups); // Output the number of groups
+    return 0;
+}
